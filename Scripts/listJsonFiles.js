b1d1122ec51e36be93eb855cf1b0beb15e042968
EscapeAltDelete <96600150+EscapeAltDelete@users.noreply.github.com>
2024-12-21 21:29:23 +0100

    Delete Scripts/listJsonFiles.js

--- Scripts/listJsonFiles.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const jsonFiles = fs.readdirSync(path.join(__dirname, '../Repos'))
-  .filter(file => file.endsWith('.json'))
-  .map(file => file.replace('.json', ''));
-
-console.log('Available JSON files:', jsonFiles);
