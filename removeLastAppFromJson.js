d0681eece95f973f1d23841ffb6d36dca1342fa8
EscapeAltDelete <96600150+EscapeAltDelete@users.noreply.github.com>
2024-12-21 19:16:51 +0100

    Add files via upload

---
+++ removeLastAppFromJson.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'Repos/test.json');
+
+const jsonFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+if (jsonFile.apps.length > 0) {
+  jsonFile.apps.pop();
+}
+
+fs.writeFileSync(filePath, JSON.stringify(jsonFile, null, 2));
