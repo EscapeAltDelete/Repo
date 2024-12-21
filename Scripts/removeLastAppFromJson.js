08ce47daa337d2e2e09a8b03812a30216bc4c234
EscapeAltDelete <96600150+EscapeAltDelete@users.noreply.github.com>
2024-12-21 21:22:46 +0100

    Update and rename removeLastAppFromJson.js to Scripts/removeLastAppFromJson.js

--- Scripts/removeLastAppFromJson.js
+++ Scripts/removeLastAppFromJson.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'Repos/TrollApps.json');
+const filePath = path.join(__dirname, '../Repos/TrollApps.json');
 
 const jsonFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
